7a416cc290c8cb65af97aab37d1f059e09003e3d
mrtcologne <43711435+mrtcologne@users.noreply.github.com>
2019-07-16 00:43:40 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express= require('express');
+const request = require('request');
+
+
+const app= express();
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));//css i eklemek icin
+
+app.get('/', (req, res)=>{
+    res.render('search');
+})
+
+app.get('/results', (req, res)=>{
+    const query=req.query.search;
+    const url=`http://www.omdbapi.com/?s=${query}&apikey=thewdb`
+    //res.send('hello, it works') send this to brwoser
+    request(url, function (error, response, body) { 
+    /* eval(require('locus'))  */ // for debugging
+    if(!error && response.statusCode==200){
+        const data= JSON.parse(body); 
+        //res.send(results['Search'][0]['Title']);
+        res.render('results',{data:data});
+      } });
+});
+
+
+app.listen(8003, ()=>{
+    console.log(`Movie app has started on http://localhost:8003`)
+});
