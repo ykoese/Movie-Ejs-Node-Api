2967d4927c1288a048112ada22ab35e1f730ee84
mrtcologne <43711435+mrtcologne@users.noreply.github.com>
2019-10-28 23:03:25 +0100

    first commit

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-const express= require('express');
+const express = require('express');
 const request = require('request');
 
 
-const app= express();
+const app = express();
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));//css i eklemek icin
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.render('search');
 })
-
-app.get('/results', (req, res)=>{
-    const query=req.query.search;
-    const url=`http://www.omdbapi.com/?s=${query}&apikey=thewdb`
+const API_KEY = ""
+app.get('/results', (req, res) => {
+    const query = req.query.search;
+    const url = `http://www.omdbapi.com/?s=${query}&apikey=${API_KEY}`
     //res.send('hello, it works') send this to brwoser
-    request(url, function (error, response, body) { 
-    /* eval(require('locus'))  */ // for debugging
-    if(!error && response.statusCode==200){
-        const data= JSON.parse(body); 
-        //res.send(results['Search'][0]['Title']);
-        res.render('results',{data:data});
-      } });
+    request(url, function (error, response, body) {
+        /* eval(require('locus'))  */ // for debugging
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body);
+            //res.send(results['Search'][0]['Title']);
+            res.render('results', { data: data });
+        }
+    });
 });
 
 
-app.listen(8003, ()=>{
+app.listen(8003, () => {
     console.log(`Movie app has started on http://localhost:8003`)
 });
